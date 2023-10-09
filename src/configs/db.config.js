dac3174d479c822922ab5743f3f123d8792b54bc
Arulmurugan-BCT <arulmurugan.s@bahwancybertek.com>
2023-10-09 14:42:10 +0530

    initial commit

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,15 +1,16 @@
 const env = process.env;
 const fs = require('fs');
+console.log("here comming");
 const db = {
-    host: env.DB_HOST,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME || 'programming_languages',
-    port: env.DB_PORT || 3306,
-    ssl: {
-      mode: 'VERIFY_IDENTITY',
-      ca: fs.readFileSync('/etc/ssl/cert.pem', 'utf-8'),
-    }
+  host: env.DB_HOST,
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME || 'programming_languages',
+  port: env.DB_PORT || 3306,
+  // ssl: {
+  //   mode: 'VERIFY_IDENTITY',
+  //   ca: fs.readFileSync('/etc/ssl/cert.pem', 'utf-8'),
+  // }
 };
 
 module.exports = db;
